960beaacc510e1ffebe98f846adeca37557d84cf
kainq596@naver.com <kainq596@naver.com>
2021-03-30 12:48:05 +0900

    랜덤 list

--- food.py
+++ food.py
@@ -1,11 +1,14 @@
+import random
 #아래 내용 전체 반복
 for i in range(1, 4):
     print(i)
 
     print("점심시간")
     print("뭐먹지")
-    print("1.한식 2.중식 3.일식")
+    menulist = '한식', '중식', '일식'
+    print("1.한식 2.중식 3.일식 4.랜덤")
     menu = input(str(i) + " 입력:")
+    
     #입력값이 1 = 한식 먹어야됨 
     #입력값이 2 = 짜장면이나 먹어야됨
     #입력값이 3 = 라멘이나 먹어야됨
@@ -15,4 +18,7 @@ for i in range(1, 4):
         print("중식")
     if menu == '3':
         print("일식")
+    if menu == '4':
+        print("랜덤")
+        print(random.choice(menulist))
 ## 여기까지 반복##
---
+++ test.py
@@ -0,0 +1,19 @@
+#import random
+#menu = '한식', '중식', '일식'
+#print(menu)
+#print(random.choice(menu))
+
+#a = '1. 쫄면'
+#b = '2. 육개장'
+#c = '3. 비빔밥'
+#d = '4. 돈까스'
+
+menulist = ['1.쫄면','2.육개장','3.비빔밥','4.돈까스']
+
+print(menulist[0:4])
+print(len(menulist))
+for i in menulist:
+    print(i)
+
+
+
